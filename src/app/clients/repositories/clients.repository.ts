5571321a658217389931eeb5b9181579f115713a
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-09 20:27:56 -0300

    feat: add create method to ClientsRepository

--- src/app/clients/repositories/clients.repository.ts
+++ src/app/clients/repositories/clients.repository.ts
@@ -1,5 +1,7 @@
 import IQuery from "../../../utils/query";
 
+import CreateClientDto from "../dtos/create.dto";
+
 export default class ClientsRepository {
   constructor() {}
 
@@ -13,6 +15,16 @@ export default class ClientsRepository {
     if (name) queryString += ` AND name LIKE '%${name}%'`;
     if (email) queryString += ` AND email LIKE '%${email}%'`;
     if (phone) queryString += ` AND phone LIKE '%${phone}%'`;
+
+    return client.query(queryString);
+  }
+
+  async create(data: CreateClientDto) {
+    const client = await connection.connect();
+
+    const { name, email, phone } = data;
+
+    const queryString = `INSERT INTO clients (name, email, phone) VALUES ('${name}', '${email}', '${phone}')`;
 
     return client.query(queryString);
   }
