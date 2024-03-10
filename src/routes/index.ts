d7670ad573998265dc5114500f2847a333d53266
Cééesar Henrique <cesaarhvb@gmail.com>
2024-03-10 07:04:33 -0300

    feat: add delivery route to index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import clientsRouter from "./clients.route";
+import deliveryRouter from "./delivery.route";
 
 const routes = Router();
 
 routes.use("/clients", clientsRouter);
+
+routes.use("/delivery", deliveryRouter);
 
 export default routes;
